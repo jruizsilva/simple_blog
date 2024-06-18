e5cc6ecab3e7b8fa46f27b5c9ec6f2791ee6a0e8
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2024-06-17 21:15:11 -0300

    asignar role user done

--- app/Livewire/Forms/EditUserForm.php
+++ app/Livewire/Forms/EditUserForm.php
@@ -11,13 +11,12 @@ class EditUserForm extends Form
     public $open;
     #[Validate([
         'roles' => [
-            'required',
             'array',
-            'min:1'
         ]
     ])]
     public $roles;
     public $name;
+    public $userId;
 
     public function edit(User $user)
     {
@@ -25,10 +24,14 @@ class EditUserForm extends Form
         $this->open = true;
         $this->roles = $user->roles()->pluck('id')->toArray();
         $this->name = $user->name;
+        $this->userId = $user->id;
     }
 
     public function update()
     {
         $this->validate();
+        $user = User::find($this->userId);
+        $user->roles()->sync($this->roles);
+        $this->reset();
     }
 }
